14c2d219aad894b93dc066ac61882293224f2d91
borislav nikolov <jack@sofialondonmoskva.com>
2019-03-15 11:15:21 +0100

    inital

---
+++ cmd/cameltosnake/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/iancoleman/strcase"
+)
+
+func main() {
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		fmt.Printf("%s\n", strcase.ToSnake(s.Text()))
+	}
+}
