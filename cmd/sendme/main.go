29c38d9c7f512a17edf4ca2c80a02014cd86f489
borislav nikolov <jack@sofialondonmoskva.com>
2019-04-08 22:28:53 +0200

    use html and text

--- cmd/sendme/main.go
+++ cmd/sendme/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strings"
 	"time"
 
@@ -13,15 +12,21 @@ import (
 )
 
 func main() {
+	var pinhtml = flag.String("html", "", "html file")
+	var pintext = flag.String("text", "", "text file")
 	var pfrom = flag.String("ffrom", "", "from")
 	var pto = flag.String("to", "", "to comma separated list")
 	var puser = flag.String("user", "", "user")
-	var pct = flag.String("content-type", "text/html", "content type")
 	var psmtp = flag.String("smtp", "smtp.gmail.com", "smtp")
 	var psbj = flag.String("subject", fmt.Sprintf("testing %d", time.Now().UnixNano()), "subject")
 	flag.Parse()
 
-	in, err := ioutil.ReadAll(os.Stdin)
+	in, err := ioutil.ReadFile(*pinhtml)
+	if err != nil {
+		panic(err)
+	}
+
+	intext, err := ioutil.ReadFile(*pintext)
 	if err != nil {
 		panic(err)
 	}
@@ -39,8 +44,9 @@ func main() {
 	}
 	m.SetHeader("To", strings.Split(*pto, ",")...)
 	m.SetHeader("Subject", *psbj)
-	m.SetBody(*pct, string(in))
-
+	m.SetBody("text/plain", string(intext))
+	m.SetHeader("Content-Transfer-Encoding", "quoted-printable")
+	m.AddAlternative("text/html", string(in))
 	d := gomail.NewPlainDialer(*psmtp, 587, *puser, pass)
 	if err := d.DialAndSend(m); err != nil {
 		panic(err)
