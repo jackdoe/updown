f126399dca723635a77fcf70509f8c5946a7f7d3
borislav nikolov <jack@sofialondonmoskva.com>
2019-07-25 13:16:49 +0200

    add one per line

--- cmd/pj/pj.go
+++ cmd/pj/pj.go
@@ -1,21 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"flag"
+	"io"
 	"os"
 )
 
 func main() {
-	jd := json.NewDecoder(os.Stdin)
+	perLine := flag.Bool("one-per-line", false, "is there one json document one per line?")
+	flag.Parse()
+
 	je := json.NewEncoder(os.Stdout)
-	var v interface{}
-	err := jd.Decode(&v)
-	if err != nil {
-		panic(err)
-	}
 	je.SetIndent("", "  ")
-	err = je.Encode(v)
-	if err != nil {
-		panic(err)
+	if !*perLine {
+		jd := json.NewDecoder(os.Stdin)
+		var v interface{}
+		err := jd.Decode(&v)
+		if err != nil {
+			panic(err)
+		}
+		err = je.Encode(v)
+		if err != nil {
+			panic(err)
+		}
+
+	} else {
+		r := bufio.NewReader(os.Stdin)
+		for {
+			data, err := r.ReadBytes('\n')
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				panic(err)
+			}
+
+			var v interface{}
+			err = json.Unmarshal(data, &v)
+			if err != nil {
+				panic(err)
+			}
+			err = je.Encode(v)
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
+
 }
