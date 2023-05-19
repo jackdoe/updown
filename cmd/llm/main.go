bce52ba4fcbbbb10c0ef32fe9cef469752c9fdb1
borislav nikolov <borislav@fastmail.com>
2023-05-19 10:53:23 +0200

    usage

--- cmd/llm/main.go
+++ cmd/llm/main.go
@@ -5,18 +5,31 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 
 	openai "github.com/sashabaranov/go-openai"
 )
 
+var Usage = func() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "  %s [options] system promopt\n\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "  Example:\n  cat file.txt | %s format as csv\n\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "  Options:\n")
+	flag.PrintDefaults()
+
+}
+
 func main() {
+	flag.Usage = Usage
+
 	model := flag.String("m", "gpt-3.5-turbo", "options: gpt-4-32k, gpt-4, gpt-3.5-turbo")
 	temp := flag.Float64("t", 0.7, "temperature")
 	fname := flag.String("f", "", "load the system prompt from a file")
 	flag.Parse()
 
+	log.Printf("%v", flag.Args())
 	ai := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
 	systemPrompt := ""
 	if len(*fname) != 0 {
